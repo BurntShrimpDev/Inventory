ff2bac59aa852a3280bfa86e59e7f70520d31de3
Callum Brogan <brogancsp@gmail.com>
2026-01-15 07:53:23 +0100

    Creating Plugin & Player Controller

---
+++ Plugins/InventorySystem/Source/InventorySystem/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,12 @@
+﻿// Copyright Callum Brogan.
+
+
+#include "Player/Inv_PlayerController.h"
+#include "InventorySystem.h"
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UE_LOG(LogInventory, Log, TEXT("BeginPlay for PlayerController"));
+}
