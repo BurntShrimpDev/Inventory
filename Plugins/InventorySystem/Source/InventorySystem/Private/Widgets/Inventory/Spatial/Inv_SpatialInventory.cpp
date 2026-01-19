fa8f8def6ed986f14ed79c93641546cdfa39b15d
Callum Brogan <brogancsp@gmail.com>
2026-01-19 06:52:38 +0100

    Switcher Buttons

--- Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
+++ Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
@@ -2,3 +2,47 @@
 
 
 #include "Widgets/Inventory/Spatial/Inv_SpatialInventory.h"
+
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "Widgets/Inventory/Spatial/Inv_InventoryGrid.h"
+
+void UInv_SpatialInventory::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	Button_Equipment->OnClicked.AddDynamic(this, &ThisClass::ShowEquipment);
+	Button_Consumables->OnClicked.AddDynamic(this, &ThisClass::ShowConsumables);
+	Button_Crafting->OnClicked.AddDynamic(this, &ThisClass::ShowCrafting);
+	
+	ShowEquipment();
+}
+
+void UInv_SpatialInventory::ShowEquipment()
+{
+	SetActiveGrid(Grid_Equipment, Button_Equipment);
+}
+
+void UInv_SpatialInventory::ShowConsumables()
+{
+	SetActiveGrid(Grid_Consumables, Button_Consumables);
+}
+
+void UInv_SpatialInventory::ShowCrafting()
+{
+	SetActiveGrid(Grid_Crafting, Button_Crafting);
+}
+
+void UInv_SpatialInventory::DisableButton(UButton* Button)
+{
+	Button_Equipment->SetIsEnabled(true);
+	Button_Consumables->SetIsEnabled(true);
+	Button_Crafting->SetIsEnabled(true);
+	Button->SetIsEnabled(false);
+}
+
+void UInv_SpatialInventory::SetActiveGrid(UInv_InventoryGrid* InventoryGrid, UButton* Button)
+{
+	DisableButton(Button);
+	
+	Switcher->SetActiveWidget(InventoryGrid);
+}
--- Plugins/InventorySystem/Source/InventorySystem/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
+++ Plugins/InventorySystem/Source/InventorySystem/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
@@ -6,6 +6,7 @@
 #include "Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
 #include "Inv_SpatialInventory.generated.h"
 
+class UButton;
 class UWidgetSwitcher;
 class UInv_InventoryGrid;
 /**
@@ -16,6 +17,8 @@ class INVENTORYSYSTEM_API UInv_SpatialInventory : public UInv_InventoryBase
 {
 	GENERATED_BODY()
 public:
+	
+	virtual void NativeOnInitialized() override;
 private:
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UWidgetSwitcher> Switcher;
@@ -28,4 +31,26 @@ private:
 	
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UInv_InventoryGrid> Grid_Crafting;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Equipment;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Consumables;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Crafting;
+	
+	UFUNCTION()
+	void ShowEquipment();
+	
+	UFUNCTION()
+	void ShowConsumables();
+	
+	UFUNCTION()
+	void ShowCrafting();
+	
+	void DisableButton(UButton* Button);
+
+	void SetActiveGrid(UInv_InventoryGrid* InventoryGrid, UButton* Button);
 };
