1a6a5fa907d3f2b60acf38c0ebe747a21951ab9d
Callum Brogan <brogancsp@gmail.com>
2026-01-25 12:50:55 +0100

    Slotted Item

---
+++ Plugins/InventorySystem/Source/InventorySystem/Public/Widgets/Inventory/SlottedItems/Inv_SlottedItem.h
@@ -0,0 +1,40 @@
+﻿// Copyright Callum Brogan.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_SlottedItem.generated.h"
+
+class UInv_InventoryItem;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInv_SlottedItem : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetIsStackable(const bool bStackable) { bIsStackable = bStackable; }
+	bool IsStackable() const { return bIsStackable; }
+	UImage* GetImageIcon() const { return Image_Icon; }
+	void SetGridIndex(const int32 Index) { GridIndex = Index; }
+	int32 GetGridIndex() const { return GridIndex; }
+	void SetGridDimensions(const FIntPoint& Dimensions) { GridDimensions = Dimensions; }
+	FIntPoint GetGridDimensions() const { return GridDimensions; }
+	void SetInventoryItem(UInv_InventoryItem* Item);
+	UInv_InventoryItem* GetInventoryItem() const { return InventoryItem.Get(); }
+	void SetImageBrush(const FSlateBrush& Brush) const;
+	
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> Image_Icon;
+
+	int32 GridIndex;
+	FIntPoint GridDimensions;
+	TWeakObjectPtr<UInv_InventoryItem> InventoryItem;
+	bool bIsStackable{false};
+};
