327ede4fd2cbb035299b974cd0546efd40b18276
Callum Brogan <brogancsp@gmail.com>
2026-02-02 22:34:34 +0100

    Assign Hover Item

--- Plugins/InventorySystem/Source/InventorySystem/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
+++ Plugins/InventorySystem/Source/InventorySystem/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
@@ -73,6 +73,9 @@ private:
 	bool IsRightClick(const FPointerEvent& MouseEvent) const;
 	bool IsLeftClick(const FPointerEvent& MouseEvent) const;
 	
+	void PickUp(UInv_InventoryItem* ClickedInventoryItem, const int32 GridIndex);
+	void AssignHoverItem(UInv_InventoryItem* InventoryItem);
+	
 	UFUNCTION()
 	void AddStacks(const FInv_SlotAvailabilityResult& Result);
 	
