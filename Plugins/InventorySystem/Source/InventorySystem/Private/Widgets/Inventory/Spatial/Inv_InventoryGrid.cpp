2e7eac25328d8886f65216da778be86db9ed632c
Callum Brogan <brogancsp@gmail.com>
2026-02-03 17:43:44 +0100

    Canvas and Mouse Position

--- Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -26,6 +26,22 @@ void UInv_InventoryGrid::NativeOnInitialized()
 	InventoryComponent->OnStackChange.AddDynamic(this, &ThisClass::AddStacks);
 }
 
+void UInv_InventoryGrid::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+	
+	const FVector2D CanvasPosition = UInv_WidgetUtils::GetWidgetPosition(CanvasPanel);
+	const FVector2D MousePosition = UWidgetLayoutLibrary::GetMousePositionOnViewport(GetOwningPlayer());
+	
+	UpdateTileParameters(CanvasPosition, MousePosition);
+}
+
+void UInv_InventoryGrid::UpdateTileParameters(const FVector2D& CanvasPosition, const FVector2D& MousePosition)
+{
+	// Calculate the tile quadrant
+	// Handle highlight/unhighlight of grid slots
+}
+
 FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const UInv_ItemComponent* ItemComponent)
 {
 	return HasRoomForItem(ItemComponent->GetItemManifest());
@@ -374,6 +390,8 @@ void UInv_InventoryGrid::RemoveItemFromGrid(UInv_InventoryItem* InventoryItem, c
 		FoundSlottedItem->RemoveFromParent();
 	}
 }
+
+
 
 void UInv_InventoryGrid::AddStacks(const FInv_SlotAvailabilityResult& Result)
 {
